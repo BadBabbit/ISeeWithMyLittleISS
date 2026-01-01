29124335dcbaab2624371e48e2b4f466908070cb
BadBabbit <nevanmasterson1@gmail.com>
2026-01-01 20:48:25 +0000

    daemon listens for event. once event is set, triggers graceful shutdown.
    database operations are now configured as a transaction.
    
    error handling is expanded to handle multiple different error types.
    
    incorporated logging.
    
    fixed issue with app context differing from that of parent thread; now uses with app.app_context() and does not configure its own g variables that the parent does not see for the purposes of event listening (that was a pretty bad hack anyway)

--- iseeiss/iss_api.py
+++ iseeiss/iss_api.py
@@ -2,19 +2,15 @@
 
     https://wheretheiss.at/w/developer
 """
-
 import requests
 import time
-import sqlite3
-import threading
-from datetime import datetime
 from flask import g
-from config import ISS_API_URL, ISS_API_TIMEOUT, ISS_API_RATE_LIMIT, ISS_MAX_ROWS
-from db import db
-import transaction
+from .config import ISS_API_URL, ISS_API_TIMEOUT, ISS_API_RATE_LIMIT, ISS_MAX_ROWS
+from .db import get_db, close_db
+from threading import Event
 
 INSERT_ISS_DATA_STATEMENT = """
-    INSERT INTO iss
+    INSERT INTO iss (timestamp, lat, lon)
     VALUES (:timestamp, :latitude, :longitude);
 """
 
@@ -38,18 +34,42 @@ class ISSApiStatusError(Exception):
     """
     pass
 
-@db
-def iss_api_daemon():
+def iss_api_daemon(app, stop_event):
     """ Daemon thread that repeatedly gets a location, adds it to the database, deletes the oldest entry if there are more than the max allowed rows in the db.
+    
+        :param app: Flask application instance
+        :param stop_event: Threading event to signal shutdown
     """
-    try:
-        response = requests.get(ISS_API_URL, timeout=ISS_API_TIMEOUT).json()
-        if response == None:
-            raise ISSApiError
-    except ISSApiError:
-        # TODO handle
-        pass
-    g.sql.execute(INSERT_ISS_DATA_STATEMENT, *response)
-    if len(list(g.sql.fetchall("SELECT * FROM iss;"))) > ISS_MAX_ROWS:
-        g.sql.execute(DELETE_ISS_DATA_STATEMENT, max_rows=ISS_MAX_ROWS)
-    time.sleep(ISS_API_RATE_LIMIT)
+    with app.app_context():
+        app.logger.info("ISS API daemon starting...")
+        get_db() # cant use decorator, as its called before the app context is set which causes errors with accessing g.
+        while not stop_event.is_set():
+            try:
+                request = requests.get(ISS_API_URL, timeout=ISS_API_TIMEOUT)
+                response = request.json()
+                if response is None:
+                    raise ISSApiTimeoutError(f"API timed out")
+                if request.status_code != 200:
+                    raise ISSApiStatusError(f"API responded with status code {request.status_code}")
+                g.sql.execute(INSERT_ISS_DATA_STATEMENT, response)
+                row_count = g.sql.execute("SELECT COUNT(*) FROM iss;").fetchone()[0]
+                if row_count > ISS_MAX_ROWS:
+                    g.sql.execute(DELETE_ISS_DATA_STATEMENT, {"max_rows": ISS_MAX_ROWS})
+                g.sql.commit()
+                app.logger.info("data successly pulled from API.")
+            except ISSApiTimeoutError as e:
+                app.logger.warning(e)
+            except ISSApiStatusError as e:
+                app.logger.error(e)
+            except (InterruptedError, KeyboardInterrupt):
+                app.logger.info("ISS API daemon interrupted")
+                g.sql.rollback()
+                break
+            except Exception as e:
+                app.logger.error(f"Unexpected ISS API daemon error: {e}")
+                g.sql.rollback()
+            
+            # we use wait instead of sleep here so we can respond to stop_event immediately
+            stop_event.wait(ISS_API_RATE_LIMIT)
+        close_db()
+        app.logger.info("ISS API daemon terminated.")
