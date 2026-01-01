44d13f0491a3bb61b9b27e6b3688d8b6def2943e
BadBabbit <nevanmasterson1@gmail.com>
2026-01-01 20:42:53 +0000

    init now: - loads logging config from yaml file - clears the existing logger - starts the ISS API daemon - registers an atexit call to gracefully shut down the daemon by setting an event which the daemon handles

--- iseeiss/__init__.py
+++ iseeiss/__init__.py
@@ -1,24 +1,50 @@
 import os
-from flask import Flask
+from flask import Flask, g
+import threading
+import atexit
+import logging
 
 def create_app(test_config=None):
+    # initial config
+    from .logging import configFromYaml
+    configFromYaml()
+    app.logger.handlers.clear() # remove flask's default handler, otherwsie we get duplicate loggers
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
+    
+    app.debug = True # TODO remove before deploying to prod
+    app.config.from_mapping( # FIXME this should come from the config file
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'iseeiss.sqlite'),
     )
     if test_config is None:
         # TODO
         pass
-    else:
+    else:   
         # TODO
         pass
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+    # initialise database
     from . import db
     db.init_app(app)
-    from views import iss
+    from .views import iss
+
+    # blueprints
     app.register_blueprint(iss.bp)
+
+    # initialise api daemon
+    from .iss_api import iss_api_daemon
+    stop_event = threading.Event()
+    t = threading.Thread(target=iss_api_daemon, name='ISS API Daemon', args=(app, stop_event), daemon=True)
+    t.start()
+    
+    # register shutdown handler that will be called on exit
+    def shutdown_daemon():
+        stop_event.set()
+        t.join(timeout=10)
+    
+    atexit.register(shutdown_daemon)
     return app
