10917075a9611d30241a52c34da4794f2d5fa070
BadBabbit <nevanmasterson1@gmail.com>
2026-01-04 20:30:35 +0000

    init files for a status light component which will indicate the status of the flask API

---
+++ frontend/src/components/APIStatusLight/APIStatusLight.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react'
+import { StatusResponse } from "../../api/status/types"
+import { fetchStatus } from "../../api/status/api"
+
+const StatusLight = () => {
+    return 
+}
+
+export default StatusLight
