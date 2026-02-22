e6a6ad3115c5d6a9634a793ee738e91513627111
BadBabbit <nevanmasterson1@gmail.com>
2026-02-22 16:55:44 +0000

    refactored server daemon to fetch TLEs from ISS api instead of lat/lon.

--- backend/iss_api.py
+++ backend/iss_api.py
@@ -10,8 +10,8 @@ from .db import get_db, close_db
 from threading import Event, current_thread
 
 INSERT_ISS_DATA_STATEMENT = """
-    INSERT INTO iss (timestamp, lat, lon)
-    VALUES (:timestamp, :latitude, :longitude);
+    INSERT INTO iss (timestamp, tle_line_1, tle_line_2)
+    VALUES (:timestamp, :tle_line_1, :tle_line_2);
 """
 
 DELETE_ISS_DATA_STATEMENT = """
@@ -34,6 +34,42 @@ class ISSApiStatusError(Exception):
     """
     pass
 
+def get_iss_tle(app, cur):
+    """ Gets the current TLE data for the ISS and writes it to the database
+
+        :param app: Flask application instance
+        :param cur: db cursor
+        :returns: None
+    """
+    try:
+        request = requests.get(ISS_API_URL + "/satellites/25544/tles", timeout=ISS_API_TIMEOUT)
+        response = request.json()
+        if response is None:
+            raise ISSApiTimeoutError(f"API timed out")
+        if request.status_code != 200:
+            raise ISSApiStatusError(f"API responded with status code {request.status_code}")
+        cur.execute(INSERT_ISS_DATA_STATEMENT, {
+            "timestamp": response["timestamp"],
+            "tle_line_1": response["line1"],
+            "tle_line_2": response["line2"]
+        })
+        row_count = cur.execute("SELECT COUNT(*) FROM iss;").fetchone()[0]
+        if row_count >= 2:
+            cur.execute(DELETE_ISS_DATA_STATEMENT, {"max_rows": ISS_MAX_ROWS})
+        cur.commit()
+        app.logger.info("TLE data successly pulled from API.")
+    except ISSApiTimeoutError as e:
+        app.logger.warning(e)
+    except ISSApiStatusError as e:
+        app.logger.error(e)
+    except (InterruptedError, KeyboardInterrupt):
+        app.logger.info("ISS API daemon interrupted")
+        cur.rollback()
+        return
+    except Exception as e:
+        app.logger.error(f"Unexpected ISS API daemon error: {e}")
+        cur.rollback()
+
 def iss_api_daemon(app, stop_event):
     """ Daemon thread that repeatedly gets a location, adds it to the database, deletes the oldest entry if there are more than the max allowed rows in the db.
     
@@ -44,32 +80,7 @@ def iss_api_daemon(app, stop_event):
         app.logger.info("ISS API daemon starting...")
         cur = get_db()
         while not stop_event.is_set():
-            try:
-                request = requests.get(ISS_API_URL, timeout=ISS_API_TIMEOUT)
-                response = request.json()
-                if response is None:
-                    raise ISSApiTimeoutError(f"API timed out")
-                if request.status_code != 200:
-                    raise ISSApiStatusError(f"API responded with status code {request.status_code}")
-                cur.execute(INSERT_ISS_DATA_STATEMENT, response)
-                row_count = cur.execute("SELECT COUNT(*) FROM iss;").fetchone()[0]
-                if row_count > ISS_MAX_ROWS:
-                    cur.execute(DELETE_ISS_DATA_STATEMENT, {"max_rows": ISS_MAX_ROWS})
-                cur.commit()
-                app.logger.info("data successly pulled from API.")
-            except ISSApiTimeoutError as e:
-                app.logger.warning(e)
-            except ISSApiStatusError as e:
-                app.logger.error(e)
-            except (InterruptedError, KeyboardInterrupt):
-                app.logger.info("ISS API daemon interrupted")
-                cur.rollback()
-                break
-            except Exception as e:
-                app.logger.error(f"Unexpected ISS API daemon error: {e}")
-                cur.rollback()
-            finally:
-                # we use wait instead of sleep here so we can respond to stop_event immediately
-                stop_event.wait(ISS_API_RATE_LIMIT)
+            get_iss_tle(app, cur)
+            stop_event.wait(ISS_API_RATE_LIMIT)
         close_db()
         app.logger.info(f"{current_thread().name} terminated.")
--- backend/schema.sql
+++ backend/schema.sql
@@ -3,6 +3,6 @@ DROP TABLE IF EXISTS iss;
 CREATE TABLE iss (
     id INTEGER PRIMARY KEY,
     timestamp INTEGER NOT NULL, --unix epoch
-    lat FLOAT NOT NULL,
-    lon FLOAT NOT NULL
+    tle_line_1 TEXT NOT NULL,
+    tle_line_2 TEXT NOT NULL
 );
