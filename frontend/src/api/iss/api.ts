4b40d3e933f9916e29404d790589da5172040c6d
BadBabbit <nevanmasterson1@gmail.com>
2026-02-22 19:09:02 +0000

    added missing "/tle" to endpoint to fetch from

--- frontend/src/api/iss/api.ts
+++ frontend/src/api/iss/api.ts
@@ -1,7 +1,7 @@
 import { IssData } from "./types";
 
 export async function fetchIssData(): Promise<IssData> {
-    const response = await fetch("/api/iss/")
+    const response = await fetch("/api/iss/tle")
     if (!response.ok) {
         throw new Error(`ISS API error: ${response.status}`);
     }
