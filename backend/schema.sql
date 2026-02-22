e4aa6644af52b9b83878377231eec53a9228a026
BadBabbit <nevanmasterson1@gmail.com>
2026-02-22 18:58:46 +0000

    added a request timestamp alongside tle timestamp in schema to allow us to discern between more recent request entries

--- backend/schema.sql
+++ backend/schema.sql
@@ -2,7 +2,8 @@ DROP TABLE IF EXISTS iss;
 
 CREATE TABLE iss (
     id INTEGER PRIMARY KEY,
-    timestamp INTEGER NOT NULL, --unix epoch
+    request_timestamp INTEGER NOT NULL, --unix epoch
+    tle_timestamp INTEGER NOT NULL, --unix epoch
     tle_line_1 TEXT NOT NULL,
     tle_line_2 TEXT NOT NULL
 );
