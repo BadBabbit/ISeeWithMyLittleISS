87d32074d7a7e203ce086077a709137f5b4e2fa1
BadBabbit <nevanmasterson1@gmail.com>
2026-04-28 20:43:19 +0100

    defined a top-level coordinates state that we will use to manage between manually defined and button-generated coordinate data

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,24 @@
 import GlobeComponent from "../components/Globe/GlobeComponent"
+import MyLocationButton from "../components/MyLocationButton/MyLocationButton"
+
+import React, { useState } from "react";
 
 export default function Home () {
+
+    const [userCoordinates, setUserCoordinates] = useState<{ lat: number; lon: number } | null>(null);
+
+    const handleLocationRetrieved = (coords: { lat: number; lon: number }) => {
+        setUserCoordinates(coords);
+        console.log("User coordinates in Home component: ", coords);
+    };
+
     return (
         <>
             <p>Hello home!</p>
             <div style={{ display: 'flex', justifyContent: 'center' }}>
                 <GlobeComponent />
             </div>
+            <MyLocationButton />
         </>
     )
 }
